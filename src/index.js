845913465e49e8ae2dfa64617f401ee948cc6a45
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2022-12-03 21:56:16 -0500

    Add code to sign up a user

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { Amplify, API, graphqlOperation } from 'aws-amplify';
+import { Amplify, API, graphqlOperation, Auth } from 'aws-amplify';
 import awsconfig from './aws-exports.js';
 import { createTodo, updateTodo, deleteTodo } from './graphql/mutations.js';
 import { listTodos } from './graphql/queries.js';
@@ -14,5 +14,23 @@ Amplify.configure(awsconfig);
 // );
 // console.log(result);
 
-const todos = await API.graphql(graphqlOperation(listTodos));
-console.log(todos.data.listTodos.items);
+// const todos = await API.graphql(graphqlOperation(listTodos));
+// console.log(todos.data.listTodos.items);
+
+const signup = async (username, email, password) => {
+  try {
+    const { user } = await Auth.signUp({
+      username,
+      password,
+      attributes: {
+        email,
+        // example: 'This is an example attribute',
+      },
+    });
+    console.log(user);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+signup('hayata', 'hayata.suenaga111@gmail.com', 'password');
